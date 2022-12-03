af6b4ad572a02c75c0ef276bbcddf8f635aeaf54
Vinícius Cabral <96541031+vini-cabral@users.noreply.github.com>
2022-12-03 10:12:18 -0300

    Improved use of environment variables

--- src/lib/sanityConfig.ts
+++ src/lib/sanityConfig.ts
@@ -1,8 +1,10 @@
+import { ENV_SANITY_AUTH_TOKEN, ENV_PUBLIC_NEXT_SANITY_PROJECT_ID } from "../env"
+
 const config = {
-  projectId: process.env.PUBLIC_NEXT_SANITY_PROJECT_ID!,
+  projectId: ENV_PUBLIC_NEXT_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-11-29",
-  token: process.env.SANITY_AUTH_TOKEN,
+  token: ENV_SANITY_AUTH_TOKEN,
   useCdn: false
 }
 
