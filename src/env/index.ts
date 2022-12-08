1906b312b1e5da211bfe9289cbd0ecad23d26ac2
Vinícius Cabral <96541031+vini-cabral@users.noreply.github.com>
2022-12-08 09:23:21 -0300

    Refactored environmental variables

---
+++ src/env/index.ts
@@ -0,0 +1,15 @@
+const PUBLIC_AUTHOR_URL="https://github.com/vini-cabral"
+const PUBLIC_AUTHOR_DESC="Vinícius Cabral"
+const PUBLIC_REPO_URL="https://github.com/vini-cabral/clone-interior-design"
+const PUBLIC_REPO_DESC="Clone Interior Design"
+const PUBLIC_REF_PROJECT_URL="https://www.w3schools.com/w3css/tryw3css_templates_interior_design.htm"
+const PUBLIC_REF_PROJECT_DESC="W3Schools Templates - Interior Design"
+
+export {
+  PUBLIC_AUTHOR_URL,
+  PUBLIC_AUTHOR_DESC,
+  PUBLIC_REPO_URL,
+  PUBLIC_REPO_DESC,
+  PUBLIC_REF_PROJECT_URL,
+  PUBLIC_REF_PROJECT_DESC
+}
--- src/layout/partials/Footer/index.tsx
+++ src/layout/partials/Footer/index.tsx
@@ -1,22 +1,30 @@
 import Link from 'next/link'
 // My App
 import styles from './styles.module.sass'
+import {
+  PUBLIC_AUTHOR_URL,
+  PUBLIC_AUTHOR_DESC,
+  PUBLIC_REPO_URL,
+  PUBLIC_REPO_DESC,
+  PUBLIC_REF_PROJECT_URL,
+  PUBLIC_REF_PROJECT_DESC 
+} from "src/env"
 
 export default function Footer() {
   return <footer className={ styles['footer'] }>
     <p>
       Desenvolvido por{" "}
-      <Link href={ process.env.NEXT_PUBLIC_AUTHOR_URL! } target={"_blank"}>
-        <cite>{ process.env.NEXT_PUBLIC_AUTHOR_DESC! }</cite>
+      <Link href={ PUBLIC_AUTHOR_URL } target={"_blank"}>
+        <cite>{ PUBLIC_AUTHOR_DESC }</cite>
       </Link>, veja o repositório:{" "}
-      <Link href={ process.env.NEXT_PUBLIC_REPO_URL! } target={"_blank"}>
-        <cite>{ process.env.NEXT_PUBLIC_REPO_DESC }</cite>
+      <Link href={ PUBLIC_REPO_URL } target={"_blank"}>
+        <cite>{ PUBLIC_REPO_DESC }</cite>
       </Link>.
     </p>
     <p>
       Projeto base:{" "}
-      <Link href={ process.env.NEXT_PUBLIC_REF_PROJECT_URL! } target={"_blank"}>
-        <cite>{ process.env.NEXT_PUBLIC_REF_PROJECT_DESC! }</cite>
+      <Link href={ PUBLIC_REF_PROJECT_URL } target={"_blank"}>
+        <cite>{ PUBLIC_REF_PROJECT_DESC }</cite>
       </Link>.
     </p>
   </footer>
